ca8e996f891e94b855f8333919c0652df055d248
leehd660 <86194199+leehd660@users.noreply.github.com>
2022-03-06 17:30:15 +0900

    22.03.06 Word Break 문제 해결. DP로 문제를 접근하니 빠르게 해결됨.

--- src/WordBreak.java
+++ src/WordBreak.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class WordBreak {
     public static void main(String[] args) {
@@ -11,51 +9,71 @@ public class WordBreak {
         wordDict.add("code");
         System.out.println(wordBreak(s,wordDict));
     }
+
     public static boolean wordBreak(String s, List<String> wordDict) {
-        boolean answer = false;
-        //
-        boolean check = true;
-        Set<String> set = new HashSet<>();
-        for (String word : wordDict) {
-            for (String ss : word.split("")) {
-                set.add(ss);
-            }
-        }
-        for (String ss : s.split("")) {
-            if (!set.contains(ss)) {
-                check = false;
-                break;
-            }
-        }
-        //
-        if (check) {
-            for (int i = 1; i <= s.length(); i++) {
-                String word = s.substring(0, i);
-                if (wordDict.contains(word)) {
-                    answer = FindWord(s, i, wordDict);
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        boolean[] dp = new boolean[s.length()];
+        for (int i=1; i<=s.length(); i++) {
+            for (int index : list) {
+                if (wordDict.contains(s.substring(index,i))) {
+                    dp[i-1] = true;
+                    list.add(i);
+                    break;
                 }
-                if (answer) break;
-            }
-            return answer;
-        }
-        else {
-            return check;
-        }
-    }
-    static boolean FindWord (String s, int index, List<String> wordDict) {
-        boolean answer = false;
-        if (index==s.length()) {
-            answer = true;
-        }
-        else {
-            for (int i = index + 1; i<= s.length(); i++) {
-                String word = s.substring(index, i);
-                if (wordDict.contains(word)) {
-                    answer = FindWord(s, i, wordDict);
-                }
-                if (answer) break;
             }
         }
-        return answer;
+        return dp[s.length()-1];
     }
+
+
+
+//    시간초과 발생
+//    public static boolean wordBreak(String s, List<String> wordDict) {
+//        boolean answer = false;
+//        //
+//        boolean check = true;
+//        Set<String> set = new HashSet<>();
+//        for (String word : wordDict) {
+//            for (String ss : word.split("")) {
+//                set.add(ss);
+//            }
+//        }
+//        for (String ss : s.split("")) {
+//            if (!set.contains(ss)) {
+//                check = false;
+//                break;
+//            }
+//        }
+//        //
+//        if (check) {
+//            for (int i = 1; i <= s.length(); i++) {
+//                String word = s.substring(0, i);
+//                if (wordDict.contains(word)) {
+//                    answer = FindWord(s, i, wordDict);
+//                }
+//                if (answer) break;
+//            }
+//            return answer;
+//        }
+//        else {
+//            return check;
+//        }
+//    }
+//    static boolean FindWord (String s, int index, List<String> wordDict) {
+//        boolean answer = false;
+//        if (index==s.length()) {
+//            answer = true;
+//        }
+//        else {
+//            for (int i = index + 1; i<= s.length(); i++) {
+//                String word = s.substring(index, i);
+//                if (wordDict.contains(word)) {
+//                    answer = FindWord(s, i, wordDict);
+//                }
+//                if (answer) break;
+//            }
+//        }
+//        return answer;
+//    }
 }
