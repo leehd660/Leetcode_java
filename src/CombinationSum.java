621c8a0ffeffe5bfcdaed3a437539869b85ed5ce
leehd660 <86194199+leehd660@users.noreply.github.com>
2022-03-15 02:00:20 +0900

    22.03.15 Combination Sum 문제 해결. dp로 간단하게 해결

---
+++ src/CombinationSum.java
@@ -0,0 +1,18 @@
+public class CombinationSum {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target+1];
+        for(int n : nums) {
+            if (n<=target) {
+                dp[n] = 1;
+            }
+        }
+        for(int i=1; i<target+1; i++) {
+            for (int n:nums) {
+                if (i-n>=0 && dp[i-n]!=0) {
+                    dp[i] += dp[i-n];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
